cf0493c6dec5479dc98fe33b29a2cd5d9cfbbf74
John Paul Ashenfelter <john@ashenfelter.com>
2026-02-11 16:09:11 -0500

    Add vite and vue

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const greeting = ref('Hello from Vue + Vite + Rails!')
+const count = ref(0)
+</script>
